8c44df036f726291d53d1074676aa585ae4f2837
Mathilde <mathilde.nadeau@hotmail.fr>
2023-07-19 23:05:27 +0200

    fix tests fonctionnels

--- tests/MessagesControllerTest.php
+++ tests/MessagesControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\tests\functional\controllers;
+namespace App\tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpClient\HttpClient;
@@ -9,8 +9,8 @@ class MessagesControllerTest extends WebTestCase
 {
     public function testIndex(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://localhost/message/');
+        $client = HttpClient::create(['verify_peer' => false]);
+        $response = $client->request('GET', 'https://localhost:8000/message/');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertJson($response->getContent());
@@ -18,8 +18,8 @@ class MessagesControllerTest extends WebTestCase
 
     public function testSend(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('POST', 'http://localhost/message/send', [
+        $client = HttpClient::create(['verify_peer' => false]);
+        $response = $client->request('POST', 'http://localhost:8000/message/send', [
             'body' => json_encode(['content' => 'Test message']),
             'headers' => ['Content-Type' => 'application/json'],
         ]);
@@ -30,8 +30,8 @@ class MessagesControllerTest extends WebTestCase
 
     public function testReceived(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://localhost/message/received');
+        $client = HttpClient::create(['verify_peer' => false]);
+        $response = $client->request('GET', 'http://localhost:8000/message/received');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertJson($response->getContent());
@@ -39,8 +39,8 @@ class MessagesControllerTest extends WebTestCase
 
     public function testRead(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://localhost/message/read/1');
+        $client = HttpClient::create(['verify_peer' => false]);        
+        $response = $client->request('GET', 'http://localhost:8000/message/read/1');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertJson($response->getContent());
@@ -48,8 +48,8 @@ class MessagesControllerTest extends WebTestCase
 
     public function testDelete(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('DELETE', 'http://localhost/message/delete/1');
+        $client = HttpClient::create(['verify_peer' => false]);   
+        $response = $client->request('DELETE', 'http://localhost:8000/message/delete/1');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertJson($response->getContent());
@@ -57,8 +57,8 @@ class MessagesControllerTest extends WebTestCase
 
     public function testSent(): void
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://localhost/message/sent');
+        $client = HttpClient::create(['verify_peer' => false]);   
+        $response = $client->request('GET', 'http://localhost:8000/message/sent');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertJson($response->getContent());
