1335a028e0e4bb07589550730326bf82b5b642b3
Mathilde <mathilde.nadeau@hotmail.fr>
2023-07-19 22:06:38 +0200

    Fix functional tests

--- tests/functional/controllers/MessageControllerTest.php
+++ tests/functional/controllers/MessageControllerTest.php
@@ -8,55 +8,55 @@ class MessageControllerTest extends WebTestCase
 {
     public function testIndex(): void
     {
-        $client = static::createClient();
+        $client = static::createKernel()->getContainer()->get('test.client');
         $client->request('GET', '/message/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Messages');
+        $this->assertJson($client->getResponse()->getContent());
     }
 
-    public function testNew(): void
+    public function testSend(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/message/new');
-
-        $form = $crawler->selectButton('Save')->form();
-        $form['message[content]'] = 'Test message';
-
-        $client->submit($form);
-
-        $this->assertResponseRedirects('/message/');
-    }
-
-    public function testShow(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/message/1');
+        $client = static::createKernel()->getContainer()->get('test.client');
+        $client->request('POST', '/message/send', [], [], [], json_encode(['content' => 'Test message']));
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Message');
+        $this->assertJson($client->getResponse()->getContent());
     }
 
-    public function testEdit(): void
+    public function testReceived(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/message/1/edit');
+        $client = static::createKernel()->getContainer()->get('test.client');
+        $client->request('GET', '/message/received');
 
-        $form = $crawler->selectButton('Save')->form();
-        $form['message[content]'] = 'Updated message';
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
+    }
 
-        $client->submit($form);
+    public function testRead(): void
+    {
+        $client = static::createKernel()->getContainer()->get('test.client');
+        $client->request('GET', '/message/read/1');
 
-        $this->assertResponseRedirects('/message/');
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
     }
 
     public function testDelete(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/message/1');
+        $client = static::createKernel()->getContainer()->get('test.client');
+        $client->request('DELETE', '/message/delete/1');
 
-        $client->submit($crawler->filter('#delete-form')->form());
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
+    }
 
-        $this->assertResponseRedirects('/message/');
+    public function testSent(): void
+    {
+        $client = static::createKernel()->getContainer()->get('test.client');
+        $client->request('GET', '/message/sent');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
     }
 }
